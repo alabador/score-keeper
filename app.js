fa8208475516bc232e3a2671d063424b15740df2
Austin <austinlabador@gmail.com>
2022-03-22 22:10:59 -0700

    added variable declarations for elements on js

---
+++ app.js
@@ -0,0 +1,8 @@
+//variable declarations
+const score = document.querySelector('#score');
+const scoreP1 = document.querySelector('#scoreP1');
+const scoreP2 = document.querySelector('#scoreP2');
+const p1 = document.querySelector('#p1');
+const p2 = document.querySelector('#p2');
+const reset = document.querySelector('#reset');
+const select = document.querySelector('select');
