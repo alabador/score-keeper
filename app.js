ff0eae83784b632496609905d8a944d0b9fe9f7c
Austin <austinlabador@gmail.com>
2022-03-24 01:19:59 -0700

    connected the conditional statements between p1,p2,and winning score. Able to detect what max score value is.

--- app.js
+++ app.js
@@ -2,36 +2,58 @@
 const score = document.querySelector('#score');
 let p1 = document.querySelector('#p1');
 let p2 = document.querySelector('#p2');
+
+let scoreP1 = document.querySelector('#scoreP1').innerText;
+let scoreP1Number = Number(scoreP1);
+let scoreP2 = document.querySelector('#scoreP2').innerText;
+let scoreP2Number = Number(scoreP2);
+
 const reset = document.querySelector('#reset');
 const select = document.querySelector('select');
+const p1Text = document.querySelector('#scoreP1');
+const p2Text = document.querySelector('#scoreP2');
 let winCon = Number(select.value); 
 
 p1.addEventListener('click', function() {
-    let scoreP1 = document.querySelector('#scoreP1').innerText;
-    let scoreP1Number = Number(scoreP1);
+    // let scoreP1 = document.querySelector('#scoreP1').innerText;
+    // let scoreP1Number = Number(scoreP1);
     scoreP1Number++;
     document.querySelector('#scoreP1').innerText = scoreP1Number;
+
+    if (scoreP1Number == winCon) {
+        p1Text.style.color = '#8dc91e';
+        p2Text.style.color = '#c13e26';
+    }
 
 })
 
 p2.addEventListener('click', function() {
-    let scoreP2 = document.querySelector('#scoreP2').innerText;
-    let scoreP2Number = Number(scoreP2);
+    // let scoreP2 = document.querySelector('#scoreP2').innerText;
+    // let scoreP2Number = Number(scoreP2);
     scoreP2Number++;
     document.querySelector('#scoreP2').innerText = scoreP2Number;
+
+    if (scoreP2Number == winCon) {
+        p1Text.style.color = '#8dc91e';
+        p2Text.style.color = '#c13e26';
+    }
 
 })
 
  reset.addEventListener('click', function() {
-    let scoreP1 = document.querySelector('#scoreP1').innerText;
-    let scoreP2 = document.querySelector('#scoreP2').innerText;
+    // let scoreP1 = document.querySelector('#scoreP1').innerText;
+    // let scoreP2 = document.querySelector('#scoreP2').innerText;
     document.querySelector('#scoreP1').innerText = "0";
     document.querySelector('#scoreP2').innerText = "0";
+    p1Text.style.color = '#000';
+    p2Text.style.color = '#000';
 
  })
 
-select.addEventListener('change', function() {
-    winCon = this.value;
+select.addEventListener('change', function(e) {
+    winCon = Number(e.target.value);
     console.log(winCon);
 })
+
+
 
